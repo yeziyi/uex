a18afe508bfcf3449a51e16500b8563ad8dd5561
“yijiajia1988 <yijiajia1988@gmail.com>
2018-04-02 17:28:41 +0800

    TODO

---
+++ app/src/main/java/com/smartapp/rootuninstaller/TODO.java
@@ -0,0 +1,11 @@
+package com.smartapp.rootuninstaller;
+
+public class TODO {
+//  TODO   优化指示器，缩小高度
+//     TODO       推上去的时候隐藏标题栏
+//  TODO  搜索过滤
+//   TODO         加上内存占用
+//   TODO 点击查看APP详情，复制信息
+//       TODO     分享APP
+//   TODO 跳转系统详情
+}
